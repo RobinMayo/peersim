69d5254c197fdf61c72d41a655dfed08ed8ac34a
pierroum <pierre.oumeddour@etu.upmc.fr>
2019-01-15 17:29:37 +0100

    Script de compilation en ligne de commande

--- Blottiere_Oumeddour-peersim/javac.sh
+++ Blottiere_Oumeddour-peersim/javac.sh
@@ -1,1 +1,1 @@
-javac -cp .;lib/djep-1.0.0.jar;lib/jep-2.3.0.jar;lib/peersim-1.0.5.jar;lib/peersim-doclet.jar; src/projetara/application/Application.java src/projetara/checkpointing/*.java src/projetara/util/*.java
+javac -classpath lib/djep-1.0.0.jar:lib/jep-2.3.0.jar:lib/peersim-1.0.5.jar:lib/peersim-doclet.jar src/projetara/application/*.java src/projetara/checkpointing/*.java src/projetara/util/*.java
